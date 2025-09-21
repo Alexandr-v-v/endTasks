70c0460afcba6c3cb5e7603ec10a6f3d6d7fb8ce
Vaneev Alexandr <Alexandr.Vaneev@endava.com>
2025-09-21 21:53:15 +0300

    Split classies into modules

--- Task 1/main.py
+++ Task 1/main.py
@@ -4,11 +4,9 @@
 
 from abc import ABC, abstractmethod
 from enum import Enum
-
-class Role(Enum):
-    Waiter = "Waiter"
-    Chef = "Chef"
-    Bartender = "Bartender"
+from Staff import *
+from Stations import *
+from MenuItems import *
 
 class OrderStatus(Enum):
     NEW = "New" # waiter create list of menu items, and transmit it to Chef and Bartender
@@ -16,140 +14,6 @@ class OrderStatus(Enum):
     READY = "Ready" # Waiter took dishes and baverages 
     SERVED = "Served" # dishes and baverages  are deliveder
     CLOSED = "Closed" # paied
-
-class Ingredient():
-    def __init__(self, name, count):
-        self.name = name
-        self.count = count
-
-# Staff -------------------------------------------------------------------
-
-class Staff(ABC):
-    def __init__(self, firstName, lastName, role: Role):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.role = role
-    
-class Waiter(Staff):
-    def __init__(self, firstName, lastName):
-        super().__init__(firstName, lastName, role = Role.Waiter)
-        self.orderItems = []
-
-     
-    def takeOrder(self):
-        print("Waiter: listening what customers want")
-
-     
-    def getOrderItems(self):
-        print("Waiter: write all in order")
-        return self.orderItems
-    
-     
-    def deliverOrder(self, order):
-        print("Waiter: serve the order")
-
-     
-    def receivePayment(self, order):
-        print("Waiter: get payment")
-
-class Chef(Staff):
-    def __init__(self, firstName, lastName):
-        super().__init__(firstName, lastName, role = Role.Chef)
-    
-     
-    def prepareOerder(self, orderItems):
-        print("Chef: I'm cooking dishes now")
-
-class Bartender(Staff):
-    def __init__(self, firstName, lastName):
-        super().__init__(firstName, lastName, role = Role.Bartender)
-    
-     
-    def prepareOerder(self, orderItems):
-        print("Bartender: I'm mixing cocktails now")
-
-
-# Station -------------------------------------------------------------------
-
-class Station(ABC):
-    def __init__(self, staffList : list[Staff]):
-        self.storageItems = []
-        self.staffList = staffList
-        # self.orders = []
-
-    @abstractmethod
-    def addStorageItems(self, items: list[Ingredient]):
-        pass
-
-    @abstractmethod
-    def useStorageItems(self, items: list[Ingredient]):
-        pass
-
-    @abstractmethod
-    def getStaffWaitingForOrders():
-        pass
-
-class Bar(Station):
-    def __init__(self, staffList : list[Staff]):
-        for employe in staffList:
-            if isinstance(employe, Chef): raise Exception("No chefs in the Bar!!!")
-        super().__init__(staffList)
-        self.showCase = []
-    
-    def addStorageItems(self, items: list[Ingredient]):
-        pass # TBD implementation
-
-    def useStorageItems(self, items: list[Ingredient]):
-        pass # TBD implementation
-    
-    def addShowcaseItem(self, items: list[Ingredient]):
-        pass # TBD implementation
-
-    def useShowcaseItem(self, items: list[Ingredient]):
-        pass # TBD implementation
-
-    def getStaffWaitingForOrders(self):
-        return self.staffList[0]
-
-class Kitchen(Station):
-    def __init__(self, staffList : list[Staff]):
-        for employe in staffList:
-            if isinstance(employe, Bartender): raise Exception("No bartender in the Bar!!!")
-        super().__init__(staffList)
-    
-    def addStorageItems(self, items: list[Ingredient]):
-        pass # TBD implementation
-
-    def useStorageItems(self, items: list[Ingredient]):
-        pass # TBD implementation
-
-    def getStaffWaitingForOrders(self):
-        return self.staffList[0]
-
-    
-# Menu items --------------------------------------------------------------
-
-class MenuItem(ABC):
-    def __init__(self, name, price, ingredients):
-        self.price = price
-        self.name = name
-        self.ingredients = ingredients
-
-class OrderItem(MenuItem): # it is better to use MenuItem as parameter for constructor then inheret it, but let it be so
-    def __init__(self, name, price, ingredients, amount, notes):
-        super().__init__(name, price, ingredients)
-        self.amount = amount
-        self.notes = notes
-
-class Dish(OrderItem):
-    def __init__(self, name, price, ingredients, amount, notes, prepared_by: Chef):
-        super().__init__(name, price, ingredients, amount, notes, prepared_by)
-        self.prepared_by = prepared_by
-
-class Beverage(OrderItem):
-    def __init__(self, name, price, ingredients, amount, notes, prepared_by: Bartender):
-        super().__init__(name, price, ingredients, amount, notes, prepared_by)
-        self.prepared_by = prepared_by
 
 # other -------------------------------------
 
@@ -176,7 +40,6 @@ class Order:
     def calcTotalPrice(self):
         self.totalPrice = sum(item.price * item.amount for item in self.orderItems)
 
-     
     def CreateBill(self):
         pass # TBD
 
@@ -191,7 +54,6 @@ class Order:
             print()
         else:
             raise Exception("The order isn't in status NEW!")
-        
      
     def prepareOrder(self):
         if self.status == OrderStatus.PREPARING:
@@ -209,8 +71,7 @@ class Order:
             print(f">>>>> Ored status: {self.status}")
             print()
         else:
-            raise Exception("The order isn't in status PREPARING!")
-        
+            raise Exception("The order isn't in status PREPARING!") 
      
     def serveOrder(self):
         if self.status == OrderStatus.READY:
@@ -221,8 +82,7 @@ class Order:
             print(f">>>>> Ored status: {self.status}")
             print()
         else:
-            raise Exception("The order isn't in status READY!")
-        
+            raise Exception("The order isn't in status READY!")   
      
     def payOrder(self):
         if self.status == OrderStatus.SERVED:
@@ -233,7 +93,6 @@ class Order:
         else:
             raise Exception("The order isn't in status SERVED!")
 
-
 if __name__ == "__main__":
     # init a restourant
     print("Initialization")
@@ -241,7 +100,6 @@ if __name__ == "__main__":
     # barStaff = [Chef("Ana", "Boe"), Chef("Bob", "Boe")]   ----------  test: should raise an exception 
     barStaff = [Bartender("Ana", "Boe"), Bartender("Bob", "Boe")]
     restaurant = Restaurant(kitchenStaff, barStaff)
-
     waiter1 = Waiter("Robert", "Polson")
     
     print("Initialization finished")
